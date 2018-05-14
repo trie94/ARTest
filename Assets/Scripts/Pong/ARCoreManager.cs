7d913ac398820966b294ce96df2a6d18cc8a70c0
Yujin Ariza <yujin.ariza@gmail.com>
2018-05-13 22:39:40 -0400

    server works?

---
+++ Assets/Scripts/Pong/ARCoreManager.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using GoogleARCore.CrossPlatform;
+using GoogleARCore.Examples.CloudAnchor;
+
+public class ARCoreManager : GameSingleton<ARCoreManager>
+{
+	#region Serialized fields
+
+    [SerializeField]
+    CloudAnchorUIController _uiController;
+	public CloudAnchorUIController uiController
+	{
+		get { return _uiController; }
+	}
+
+    [SerializeField]
+    GameObject _arCoreRoot;
+    [SerializeField]
+    GameObject _arKitRoot;
+
+    /// <summary>
+    /// The first-person camera used to render the AR background texture for ARKit.
+    /// </summary>
+	[SerializeField]
+    Camera _arKitFirstPersonCamera;
+
+    #endregion
+
+    #region Cloud Anchor fields
+
+    /// <summary>
+    /// True if the app is in the process of quitting due to an ARCore connection error, otherwise false.
+    /// </summary>
+    private bool m_IsQuitting = false;
+
+	/// <summary>
+    /// A helper object to ARKit functionality.
+    /// </summary>
+	private ARKitHelper _arKitHelper = new ARKitHelper();
+	public ARKitHelper arKitHelper
+	{
+		get { return _arKitHelper; }
+	}
+    
+    #endregion
+
+    #region Monobehaviour functions
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Application.platform != RuntimePlatform.IPhonePlayer)
+        {
+            _arCoreRoot.SetActive(true);
+            _arKitRoot.SetActive(false);
+        }
+        else
+        {
+            _arCoreRoot.SetActive(false);
+            _arKitRoot.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _UpdateApplicationLifecycle();      
+    }
+
+	#endregion
+
+	#region Public functions
+
+	public bool RaycastTouch(float x, float y, out Pose pose, out Trackable trackable)
+	{
+		if (Application.platform != RuntimePlatform.IPhonePlayer)
+		{
+			TrackableHit hit;
+			if (Frame.Raycast(x, y, TrackableHitFlags.PlaneWithinPolygon, out hit))
+			{
+				pose = hit.Pose;
+				trackable = hit.Trackable;
+				return true;
+			}
+        }
+		else
+		{
+			Pose hitPose;
+            if (_arKitHelper.RaycastPlane(_arKitFirstPersonCamera, x, y, out hitPose))
+			{
+				pose = hitPose;
+				trackable = null;
+				return true;
+			}
+		}
+		pose = new Pose();
+		trackable = null;
+		return false;
+	}
+
+	public Component CreateAnchor(Pose pose, Trackable trackable)
+    {
+        if (Application.platform != RuntimePlatform.IPhonePlayer)
+        {
+            return trackable.CreateAnchor(pose);
+        }
+        else
+        {
+            return _arKitHelper.CreateAnchor(pose);
+        }
+    }
+
+    #endregion
+
+	#region Cloud Anchor Behaviours
+
+    /// <summary>
+    /// Check and update the application lifecycle.
+    /// </summary>
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        var sleepTimeout = SleepTimeout.NeverSleep;
+
+#if !UNITY_IOS
+            // Only allow the screen to sleep when not tracking.
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                const int lostTrackingSleepTimeout = 15;
+                sleepTimeout = lostTrackingSleepTimeout;
+            }
+#endif
+
+        Screen.sleepTimeout = sleepTimeout;
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Actually quit the application.
+    /// </summary>
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Show an Android toast message.
+    /// </summary>
+    /// <param name="message">Message string to show in the toast.</param>
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                    message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/Pong/CloudAnchorNetworkController.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using GoogleARCore;
+using GoogleARCore.CrossPlatform;
+using GoogleARCore.Examples.CloudAnchor;
+
+public class CloudAnchorNetworkController : NetworkBehaviour
+{
+	#region Serialized fields
+
+    [SerializeField]
+    GameObject _andyPrefab;
+
+	#endregion
+
+	#region SyncVar fields
+
+	[SyncVar(hook = "OnChangeAnchor")]
+	bool _anchorFound;
+
+	[SyncVar]
+	string _anchorId;
+
+	#endregion
+
+	#region Private fields
+
+	#endregion
+
+	#region Cloud Anchor fields
+
+	/// <summary>
+    /// The last placed anchor.
+    /// </summary>
+    private Component m_LastPlacedAnchor = null;
+
+    /// <summary>
+    /// The last resolved anchor.
+    /// </summary>
+    private XPAnchor m_LastResolvedAnchor = null;
+
+	#endregion
+
+	#region Monobehaviour functions
+
+	private void Start()
+	{
+		_ResetStatus();
+
+		if (isServer)
+		{
+		}
+		else
+		{
+			// check if anchor is available
+            if (_anchorFound)
+			{
+				_ResolveAnchorFromId(_anchorId);
+			}
+		}
+    }
+
+	private void Update()
+	{
+		if (isServer)
+		{
+			ProcessTouch();
+		}
+	}
+
+	#endregion
+
+	#region Public functions
+
+	#endregion
+
+	#region Private functions
+
+    private void ProcessTouch()
+	{
+        // if there's already an anchor, skip
+		if (m_LastPlacedAnchor != null)
+		{
+			return;
+		}
+
+		// If the player has not touched the screen then the update is complete.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+		Pose pose;
+		Trackable trackable;
+
+		if (ARCoreManager.Instance.RaycastTouch(touch.position.x, touch.position.y, out pose, out trackable))
+		{
+			m_LastPlacedAnchor = ARCoreManager.Instance.CreateAnchor(pose, trackable);
+		}
+
+		if (m_LastPlacedAnchor != null)
+		{
+			var andy = Instantiate(
+				_andyPrefab,
+				m_LastPlacedAnchor.transform.position,
+				m_LastPlacedAnchor.transform.rotation);
+
+			// Compensate for the hitPose rotation facing away from the raycast (i.e. camera).
+            andy.transform.Rotate(0, 180, 0, Space.Self);
+
+            // Make Andy model a child of the anchor.
+            andy.transform.parent = m_LastPlacedAnchor.transform;
+
+            // Save cloud anchor.
+            _HostLastPlacedAnchor();
+		}
+	}
+
+	#endregion
+
+	#region SyncVar functions
+
+	void OnChangeAnchor(bool found)
+	{
+		if (isServer) return;
+
+		if (found)
+		{
+			Debug.Log("anchor found: " + _anchorId);
+			_ResolveAnchorFromId(_anchorId);
+		}
+	}
+
+	#endregion
+
+    #region Cloud Anchor functions
+
+	/// <summary>
+	/// Resets the internal status and UI.
+	/// </summary>
+	private void _ResetStatus()
+    {
+        // Reset internal status.
+        if (m_LastPlacedAnchor != null)
+        {
+            Destroy(m_LastPlacedAnchor.gameObject);
+        }
+
+        m_LastPlacedAnchor = null;
+        if (m_LastResolvedAnchor != null)
+        {
+            Destroy(m_LastResolvedAnchor.gameObject);
+        }
+
+        m_LastResolvedAnchor = null;
+		ARCoreManager.Instance.uiController.ShowReadyMode();
+    }
+
+	/// <summary>
+    /// Hosts the user placed cloud anchor and associates the resulting Id with the current room.
+    /// </summary>
+    private void _HostLastPlacedAnchor()
+    {
+#if !UNITY_IOS
+            var anchor = (Anchor)m_LastPlacedAnchor;
+#else
+        var anchor = (UnityEngine.XR.iOS.UnityARUserAnchorComponent)m_LastPlacedAnchor;
+#endif
+		ARCoreManager.Instance.uiController.ShowHostingModeAttemptingHost();
+        XPSession.CreateCloudAnchor(anchor).ThenAction(result =>
+        {
+            if (result.Response != CloudServiceResponse.Success)
+            {
+				ARCoreManager.Instance.uiController.ShowHostingModeBegin(
+                    string.Format("Failed to host cloud anchor: {0}", result.Response));
+                return;
+            }
+
+			_anchorId = result.Anchor.CloudId;
+			_anchorFound = true;
+			Debug.Log("hosting anchor id " + _anchorId);
+
+			ARCoreManager.Instance.uiController.ShowHostingModeBegin("Cloud anchor was created and saved.");
+        });
+    }
+
+	/// <summary>
+    /// Resolves an anchor id and instantiates an Andy prefab on it.
+    /// </summary>
+    /// <param name="cloudAnchorId">Cloud anchor id to be resolved.</param>
+    private void _ResolveAnchorFromId(string cloudAnchorId)
+    {
+		XPSession.ResolveCloudAnchor(cloudAnchorId).ThenAction((System.Action<CloudAnchorResult>)(result =>
+        {
+            if (result.Response != CloudServiceResponse.Success)
+            {
+				ARCoreManager.Instance.uiController.ShowResolvingModeBegin(string.Format("Resolving Error: {0}.", result.Response));
+                return;
+            }
+
+            m_LastResolvedAnchor = result.Anchor;
+
+			Debug.Log("resolved anchor " + m_LastResolvedAnchor);
+            
+            // do somethign better
+            transform.SetPositionAndRotation(result.Anchor.transform.position, result.Anchor.transform.rotation);
+            Instantiate(_andyPrefab, result.Anchor.transform);
+
+			ARCoreManager.Instance.uiController.ShowResolvingModeSuccess();
+        }));
+    }
+
+	#endregion
+}
